66ae491b979b787fddb1eb77ea6fa13fb5f4e337
yuu735 <yuu981130@gmail.com>
2021-08-24 16:48:23 +0800

    init

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // publicPath: '/dist/',
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/WorkShop-vuecli-practice/'
+    ? '/WorkShop-vuecli-practice/dist/'
     : '/',
 };
